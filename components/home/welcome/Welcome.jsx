e2b64e5f6dcaa0fe25d22c2e2a4708645c20e7a8
Amani Musomba <amusomba@gmail.com>
2024-01-04 16:22:55 +0300

    Update Welcome Text

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -22,7 +22,7 @@ const Welcome = ({ searchTerm, setSearchTerm, handleClick }) => {
   return (
     <View>
       <View style={styles.container}>
-        <Text style={styles.userName}>Hello Adrian</Text>
+        <Text style={styles.userName}>Hello John Doe</Text>
         <Text style={styles.welcomeMessage}>Find your perfect job</Text>
       </View>
 
